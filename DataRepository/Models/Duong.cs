711f7f4f7daa8fc2c8b05c003acb663c48edefe0
unknown <tranthainhu9@gmai.com>
2021-01-09 15:25:54 +0700

    data + tinhtrang controller

---
+++ DataRepository/Models/Duong.cs
@@ -0,0 +1,20 @@
+﻿using DataRepository;
+using PTUDFinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataRepository.Models
+{
+    public class Duong
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string TenDuong { get; set; }
+        public virtual ICollection<KhuVuc> KhuVucs { get; set; } = new HashSet<KhuVuc>();
+        public virtual ICollection<TinhTrang> TinhTrangs { get; set; } = new HashSet<TinhTrang>();
+    }
+}
---
+++ DataRepository/Models/TinhTrang.cs
@@ -0,0 +1,19 @@
+﻿using DataRepository;
+using PTUDFinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataRepository.Models
+{
+    public class TinhTrang
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string TrangThai { get; set; }
+        public virtual ICollection<Duong> Duongs { get; set; } = new HashSet<Duong>();
+    }
+}
