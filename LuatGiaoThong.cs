4149bb45c682ce92a0af4143ffafcde3437bce69
PanChaMature <55944744+PanChaMature@users.noreply.github.com>
2021-01-11 00:47:04 +0700

    Update LuatGiaoThong.cs

--- LuatGiaoThong.cs
+++ LuatGiaoThong.cs
@@ -9,10 +9,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace DataRepository { 
     public class LuatGiaoThong
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string TieuDeLuat { get; set; }
         public string NoiDungLuat { get; set; }
+        public int LanCapNhat { get; set; }
+        public string NgayCapNhat { get; set; }
+        public int MucPhat { get; set; }
         public ICollection<ViPhamLuatGT> ViPhamLuatGTs { get; set; }
 
     }
